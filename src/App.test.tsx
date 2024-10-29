b948ef3975d2d5eaf21cc0945446af7a8775749a
Mardon07 <mardonberdiyev10@gmail.com>
2024-10-30 01:04:54 +0500

    feat: add store

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('App Component', () => {
-  test('renders the main heading', () => {
+describe("App Component", () => {
+  test("renders the main heading", () => {
     render(<App />);
-    const headingElement = screen.getByText(/Список элементов с бесконечным скроллом/i);
+    const headingElement = screen.getByText(
+      /Список элементов с бесконечным скроллом/i
+    );
     expect(headingElement).toBeInTheDocument();
   });
 
-  test('renders the ItemList component', () => {
+  test("renders the ItemList component", () => {
     render(<App />);
-    const itemListElement = screen.getByTestId('item-list'); 
+    const itemListElement = screen.getByTestId("item-list");
     expect(itemListElement).toBeInTheDocument();
   });
 });
---
+++ src/store/itemStore.ts
@@ -0,0 +1,35 @@
+import { makeAutoObservable } from "mobx";
+import axios from "axios";
+import { Item } from "./item";
+
+class ItemStore {
+  items: Item[] = [];
+  page = 1;
+  loading = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  async fetchItems() {
+    if (this.loading) return;
+    this.loading = true;
+    try {
+      const response = await axios.get(
+        `https://api.github.com/search/repositories?q=javascript&sort=stars&order=asc&page=${this.page}`
+      );
+      this.items = [...this.items, ...response.data.items];
+      this.page += 1;
+    } catch (error) {
+      console.error("Ошибка при загрузке данных", error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  deleteItem(id: number) {
+    this.items = this.items.filter((item) => item.id !== id);
+  }
+}
+
+export default new ItemStore();
